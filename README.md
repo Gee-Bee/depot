cd29aff327ced63c1307b455b40316740e1e565c
Gee Bee <greg.bunia@gmail.com>
2014-04-15 23:30:18 +0200

    Depot Scaffold.

---
+++ README.md
@@ -0,0 +1,1 @@
+Shopping cart app implemented along reading excellent `Agile Web Development with Rails 4` book written by Sam Ruby.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '65e0fdc46a20810abd1ef8f3c6195de5f26071ecdf79ce01c93db6ed9be8cab10b81df4fce04b3ddc75c0a9efc82a6b85f242cdf206a32505bb3c2e4121d91b8'
